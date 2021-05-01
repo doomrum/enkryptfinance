4d1b8e38d99d3dca5aa3ed2917869cd52352305a
NEXTink <okechukwuomeh3@gmail.com>
2021-05-01 13:37:26 +0100

    dash

--- routes/client.js
+++ routes/client.js
@@ -9,5 +9,20 @@ router.get('/invest',(req,res,next)=>{
 router.get('/wallet',(req,res,next)=>{
     res.render('client/wallet',{layout: 'client', title:'EnkryptFinance | wallet'});
 });
+router.get('/ticket',(req,res,next)=>{
+    res.render('client/ticket',{layout: 'client', title:'EnkryptFinance | Invest'});
+});
+// router.get('/transaction',(req,res,next)=>{
+//     res.render('client/walletTransaction',{layout: 'client', title:'EnkryptFinance | wallet'});
+// });
+router.get('/profile',(req,res,next)=>{
+    res.render('client/profile',{layout: 'client', title:'EnkryptFinance | wallet'});
+});
+router.get('/referral',(req,res,next)=>{
+    res.render('client/referral',{layout: 'client', title:'EnkryptFinance | Invest'});
+});
+router.get('/withdraw',(req,res,next)=>{
+    res.render('client/withdraw',{layout: 'client', title:'EnkryptFinance | wallet'});
+});
 
 module.exports = router;
