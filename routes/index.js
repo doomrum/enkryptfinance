b67aaae5a3cfb5538d5e9173d97cac54e27ae75c
NEXTink <okechukwuomeh3@gmail.com>
2021-05-15 15:42:47 +0100

    ch

--- routes/index.js
+++ routes/index.js
@@ -9,33 +9,37 @@ router.get('/',async function(req, res, next) {
 
 router.get('/about',async function(req, res, next) {
 
-    res.render('about',{title:'EnkryptFinance | Landing Page', layout: 'index'})
+    res.render('about',{title:'EnkryptFinance | About', layout: 'index'})
+});
+router.get('/terms',async function(req, res, next) {
+
+    res.render('terms',{title:'EnkryptFinance |Terms', layout: 'index'})
 });
 
 router.get('/faqs',async function(req, res, next) {
 
-    res.render('faqs',{title:'EnkryptFinance | Landing Page', layout: 'index'})
+    res.render('faqs',{title:'EnkryptFinance | FAQs', layout: 'index'})
 });
 
 router.get('/plans',async function(req, res, next) {
 
-    res.render('plans',{title:'EnkryptFinance | Landing Page', layout: 'index'})
+    res.render('plans',{title:'EnkryptFinance | Plans', layout: 'index'})
 });
 
 
 router.get('/security',async function(req, res, next) {
 
-    res.render('security',{title:'EnkryptFinance | Landing Page', layout: 'index'})
+    res.render('security',{title:'EnkryptFinance | Security', layout: 'index'})
 });
 
 router.get('/offers',async function(req, res, next) {
 
-    res.render('offers',{title:'EnkryptFinance | Landing Page', layout: 'index'})
+    res.render('offers',{title:'EnkryptFinance | Offers', layout: 'index'})
 });
 
 router.get('/gettingstarted',async function(req, res, next) {
 
-    res.render('gettingStarted',{title:'EnkryptFinance | Landing Page', layout: 'index'})
+    res.render('gettingStarted',{title:'EnkryptFinance | Getting Started', layout: 'index'})
 });
 
 module.exports = router;
