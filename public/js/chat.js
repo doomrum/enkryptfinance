bcdcf5a10c156aa79bb2207dc09a410c576f7ff0
mr-emeka <speak2c.emeka@gmail.com>
2021-05-26 09:01:04 +0100

    (wip): ui changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,18 @@
-const chatPopup = document.getElementById('chat_area');
-const chatPopupClose = document.getElementById('toggleChatBtn');
-const msgBtn = document.getElementById('msg_btn');
+const chatPopup = document.getElementById("chat_area");
+const chatPopupClose = document.getElementById("toggleChatBtn");
+const msgBtn = document.getElementById("msg_btn");
 
-chatPopupClose.addEventListener('click',()=>{
-    chatPopup.style.display = 'none';
+chatPopupClose.addEventListener("click", () => {
+  chatPopup.style.display = "none";
+});
 
-})
-
-msgBtn.addEventListener('click',()=>{
-    if (chatPopup.style.display==='flex'){
-        chatPopup.style.display = 'none';
-    }
-    else{
-        chatPopup.style.display = 'flex';
-    }
-
-})
+msgBtn.addEventListener("click", () => {
+  if (chatPopup.style.display === "flex") {
+    chatPopup.style.display = "none";
+  } else {
+    chatPopup.style.display = "flex";
+  }
+});
 
 ////CHAT FUNCTIONALITY
 //
@@ -23,23 +20,21 @@ msgBtn.addEventListener('click',()=>{
 //
 
 /*
-*
-* a user is directed to the url
-* if user is signed in
-* a cookie is sent to the server
-* the server interprets it to get the username
-* the server then creates a group for the user & the admin
-* as messages are sent it is stored in the database as chats under a chat id
-* if the user is anonymous it is store temporarily and the persons email is required before chat begins
-*
-*
-*
-* */
+ *
+ * a user is directed to the url
+ * if user is signed in
+ * a cookie is sent to the server
+ * the server interprets it to get the username
+ * the server then creates a group for the user & the admin
+ * as messages are sent it is stored in the database as chats under a chat id
+ * if the user is anonymous it is store temporarily and the persons email is required before chat begins
+ *
+ *
+ *
+ * */
 
-
-
-const clientMsg = (msg)=>{
-    return  `
+const clientMsg = (msg) => {
+  return `
 
             <div class="chat_msg-client_title ">
               <h5 class="h6">${msg.username}</h5>
@@ -48,8 +43,8 @@ const clientMsg = (msg)=>{
             <p>${msg.text}</p>
 
 `;
-}
-const masterMsg = (msg)=> `
+};
+const masterMsg = (msg) => `
 
             <div class="chat_msg-support_title ">
                 <h5 class="h6">${msg.username}</h5>
@@ -60,43 +55,40 @@ const masterMsg = (msg)=> `
 `;
 
 ///CHART BODY
-const chat_area = document.getElementById('chat_area-body');
+const chat_area = document.getElementById("chat_area-body");
 const socket = io();
 ///SOCKET CONNECTION
-const chat_form = document.getElementById('chat_form');
+const chat_form = document.getElementById("chat_form");
 
-const chatMsg  = document.querySelector('.chat_msg-support');
+const chatMsg = document.querySelector(".chat_msg-support");
 
-
-socket.on('message',msg=>{
-    //create div
-    if(msg.username==='Admin'){
-        const div = document.createElement('div');
-        div.setAttribute('class','chat_msg-support');
-        div.innerHTML = masterMsg(msg);
-        chat_area.appendChild(div);
-        chat_area.scrollTop = chat_area.scrollHeight;
-    }
-    else { const div = document.createElement('div');
-        div.setAttribute('class','chat_msg-client');
-        div.innerHTML = clientMsg(msg);
-        chat_area.appendChild(div);
-        chat_area.scrollTop = chat_area.scrollHeight;}
+socket.on("message", (msg) => {
+  //create div
+  if (msg.username === "Admin") {
+    const div = document.createElement("div");
+    div.setAttribute("class", "chat_msg-support");
+    div.innerHTML = masterMsg(msg);
+    chat_area.appendChild(div);
+    chat_area.scrollTop = chat_area.scrollHeight;
+  } else {
+    const div = document.createElement("div");
+    div.setAttribute("class", "chat_msg-client");
+    div.innerHTML = clientMsg(msg);
+    chat_area.appendChild(div);
+    chat_area.scrollTop = chat_area.scrollHeight;
+  }
 });
 
+chat_form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-chat_form.addEventListener('submit',(e)=>{
-    e.preventDefault();
+  const message = {
+    username: "okechukwu",
+    text: e.target.elements.message.value,
+  };
 
-    const message = {
-        username:'okechukwu',
-        text:e.target.elements.message.value,
-    }
+  socket.emit("userMessage", message);
 
-    socket.emit('userMessage',message);
-
-
-    e.target.elements.message.value = '';
-    e.target.elements.message.focus();
-
-})
+  e.target.elements.message.value = "";
+  e.target.elements.message.focus();
+});
--- routes/client.js
+++ routes/client.js
@@ -1,258 +1,297 @@
-const router = require('express').Router();
-const userModel  = require('../models/user');
-const ticketModel  = require('../models/supportTicket');
-const referralModel  = require('../models/referral');
-const transactionModel  = require('../models/transaction');
-const balanceModel = require('../models/balance');
-const planModel = require('../models/plan');
-const qr = require('qrcode');
+const router = require("express").Router();
+const userModel = require("../models/user");
+const ticketModel = require("../models/supportTicket");
+const referralModel = require("../models/referral");
+const transactionModel = require("../models/transaction");
+const balanceModel = require("../models/balance");
+const planModel = require("../models/plan");
+const qr = require("qrcode");
 // const cryptoData  = require('../helpers/userInfo');
-const {sendEmail} = require('../helpers/nodemailer');
-const moment = require('moment');
-router.all('/*',(req,res,next)=>{
-    if (req.session.accessType !=='client'){
-        res.redirect('/admin');
-    }
-    else {
-            next();
-    }
-
-})
-
-router.get('/',async(req,res,next)=>{
-
-    //sessions IDs are stored in the user browser as cookies
-    //when a request s sent and there's no cookie with the session ID it will result in the failure of the request
-
-    const ID  = req.session.access;
-    const IDType  = req.session.accessType;
-
-         await userModel.findOne({_id:ID})
-             .populate('transactions')
-             .populate({path:'balance',model:'balances'})
-             .populate({path:'plan',model: 'plans'})
-             .then(user=>{
-
-                 const userInfo  = user.toJSON();
-                 console.log(user.plan)
-                 // console.log(user)
-                 const fullName = req.app.locals.username;
-                 res.render('client/index',{layout: 'client', title:'EnkryptFinance | Client',userInfo,fullName });
-             })
-
-             .catch(err=>err)
-
+const { sendEmail } = require("../helpers/nodemailer");
+const moment = require("moment");
+router.all("/*", (req, res, next) => {
+  if (req.session.accessType !== "client") {
+    res.redirect("/admin");
+  } else {
+    next();
+  }
 });
 
+router.get("/", async (req, res, next) => {
+  //sessions IDs are stored in the user browser as cookies
+  //when a request s sent and there's no cookie with the session ID it will result in the failure of the request
 
-router.get('/invest',async (req,res,next)=>{
+  const ID = req.session.access;
+  const IDType = req.session.accessType;
+
+  await userModel
+    .findOne({ _id: ID })
+    .populate("transactions")
+    .populate({ path: "balance", model: "balances" })
+    .populate({ path: "plan", model: "plans" })
+    .then((user) => {
+      const userInfo = user.toJSON();
+      console.log(user.plan);
+      // console.log(user)
+      const fullName = req.app.locals.username;
+      res.render("client/index", {
+        layout: "client",
+        title: "EnkryptFinance | Client",
+        userInfo,
+        fullName,
+      });
+    })
+
+    .catch((err) => err);
+});
+
+router.get("/invest", async (req, res, next) => {
+  const fullName = req.app.locals.username;
+  planModel
+    .find({})
+    .lean()
+    .then((plan) => {
+      res.render("client/invest", {
+        layout: "client",
+        title: "EnkryptFinance | Invest",
+        fullName,
+        plan,
+      });
+    })
+    .catch((err) => err);
+});
+
+router.get("/wallet", async (req, res, next) => {
+  transactionModel
+    .find({ owner: req.session.access })
+    .lean()
+    .then((transactions) => {
+      balanceModel
+        .findOne({ owner: req.session.access })
+        .then((bal) => {
+          console.log(transactions);
+          console.log(bal);
+          const fullName = req.app.locals.username;
+          res.render("client/wallet", {
+            layout: "client",
+            title: "EnkryptFinance | wallet",
+            transactions,
+            btcBalance: bal.btcBalance,
+            fullName,
+          });
+        })
+        .catch((err) => err);
+    })
+    .catch((err) => err);
+});
+router.get("/ticket", (req, res, next) => {
+  ticketModel
+    .find({})
+    .lean()
+    .then((t) => {
+      function ticketSort(t) {
+        const tickets = [];
+        for (let j = 0; j < t.length; j++) {
+          const ticket = {
+            subject: t[j].subject,
+            msg: t[j].msg,
+            date: moment(t[j].date).format("MMM Do YY"),
+          };
+          tickets.push(ticket);
+        }
+        return tickets;
+      }
+
+      console.log(ticketSort(t));
+      const fullName = req.app.locals.username;
+      res.render("client/ticket", {
+        layout: "client",
+        title: "EnkryptFinance | Support Ticket",
+        tickets: ticketSort(t),
+        fullName,
+      });
+    })
+    .catch((err) => res.send(err));
+});
+router.post("/ticket/create", (req, res, next) => {
+  const newTicket = ticketModel({
+    subject: req.body.subject,
+    msg: req.body.msg,
+    owner: req.session.access,
+  });
+  newTicket
+    .save()
+    .then((t) => {
+      userModel.findOne({ _id: req.session.access }).then((user) => {
+        user.supporttickets.push(newTicket);
+        user.save().then((t) => {
+          res.redirect("/client/ticket");
+        });
+      });
+    })
+    .catch((err) => err);
+});
+
+router.get("/profile", (req, res, next) => {
+  const ID = req.session.access;
+
+  userModel.findOne({ _id: ID }).then((user) => {
+    console.log(`${user} s here`);
+    const userInfo = user.toJSON();
     const fullName = req.app.locals.username;
-    planModel.find({})
-        .lean()
-        .then(plan=>{
-            res.render('client/invest',{layout: 'client', title:'EnkryptFinance | Invest',fullName,plan});
-        })
-        .catch(err=>err)
+    res.render("client/profile", {
+      layout: "client",
+      title: "EnkryptFinance | profile",
+      user: userInfo,
+      fullName,
+    });
+  });
 });
+router.get("/profile/edit/:_id", (req, res, next) => {
+  const ID = req.session.access;
 
-
-
-router.get('/wallet',async (req,res,next)=>{
-
-     transactionModel.find({owner:req.session.access})
-         .lean()
-         .then(transactions=>{
-             balanceModel.findOne({owner:req.session.access})
-                 .then(bal=>{
-                     console.log(transactions);
-                     console.log(bal);
-                     const fullName = req.app.locals.username;
-                     res.render('client/wallet',{layout: 'client', title:'EnkryptFinance | wallet',transactions ,btcBalance:bal.btcBalance,fullName});
-                 })
-                 .catch(err=>err);
-
-         })
-         .catch(err=>err)
-
-
-});
-router.get('/ticket',(req,res,next)=>{
-     ticketModel.find({})
-         .lean()
-         .then(t=>{
-
-
-             function ticketSort(t){
-                 const tickets = [];
-                 for (let j=0;j<t.length;j++){
-                     const ticket = {
-                         subject:t[j].subject,
-                         msg:t[j].msg,
-                         date: moment(t[j].date).format('MMM Do YY')
-                     }
-                     tickets.push(ticket);
-                 }
-                 return tickets;
-
-             }
-
-             console.log(ticketSort(t));
-             const fullName = req.app.locals.username;
-             res.render('client/ticket',{layout: 'client', title:'EnkryptFinance | Support Ticket',tickets:ticketSort(t),fullName});
-         })
-         .catch(err=> res.send(err))
-});
-router.post('/ticket/create',(req,res,next)=>{
-    const newTicket = ticketModel({subject:req.body.subject,msg:req.body.msg,owner:req.session.access});
-    newTicket.save()
-        .then(t=>{
-            userModel.findOne({_id:req.session.access})
-                .then(user=>{
-                    user.supporttickets.push(newTicket);
-                    user.save()
-                        .then(t=>{
-                            res.redirect('/client/ticket');
-                    })
-
-                })
-
-        })
-        .catch(err=>err)
-
-
-});
-
-router.get('/profile',(req,res,next)=>{
-    const ID  = req.session.access;
-
-    userModel.findOne({_id:ID}).then(user=>{
-        console.log(`${user} s here`);
-        const userInfo  = user.toJSON();
-        const fullName = req.app.locals.username;
-        res.render('client/profile',{layout: 'client', title:'EnkryptFinance | profile',user:userInfo, fullName});
-
+  userModel
+    .findOne({ _id: ID })
+    .then((user) => {
+      const userInfo = user.toJSON();
+      const fullName = req.app.locals.username;
+      res.render("client/editProfile", {
+        layout: "client",
+        title: "EnkryptFinance | Edit profile",
+        user: userInfo,
+        fullName,
+      });
     })
-
+    .catch((err) => res.send(err));
 });
-router.get('/profile/edit/:_id',(req,res,next)=>{
-    const ID  = req.session.access;
 
-    userModel.findOne({_id:ID})
-        .then(user=>{
-        const userInfo  = user.toJSON();
-        const fullName = req.app.locals.username;
-        res.render('client/editProfile',{layout: 'client', title:'EnkryptFinance | Edit profile',user:userInfo, fullName});
+router.post("/profile/edit/:_id", (req, res, next) => {
+  const ID = req.params._id;
 
+  userModel
+    .updateOne(
+      { _id: ID },
+      {
+        fullName: req.body.fullName,
+        email: req.body.email,
+        phone: req.body.phone,
+      }
+    )
+    .then((user) => {
+      res.redirect("/client/");
     })
-        .catch(err=>res.send(err));
-
+    .catch((err) => {
+      res.send(err);
+    });
 });
-
-router.post('/profile/edit/:_id',(req,res,next)=>{
-    const ID  = req.params._id;
-
-         userModel.updateOne({_id:ID}, {fullName:req.body.fullName, email: req.body.email, phone:req.body.phone})
-             .then(user=>{
-
-                 res.redirect('/client/');
-             })
-             .catch(err=>{
-                 res.send(err)
-             });
-
-
-
-
-
+router.get("/referral", (req, res, next) => {
+  userModel.findOne({ _id: req.session.access }).then((user) => {
+    const newReferralLink = `http://:4000${req.baseUrl}/r/${
+      user.fullName.split(" ")[0]
+    }`;
+    const newReferral = referralModel({
+      link: newReferralLink,
+      owner: req.session.access,
+    });
+    newReferral
+      .save()
+      .then((ref) => {
+        user.referralUrl = newReferralLink;
+        user
+          .save()
+          .then((r) => {
+            const fullName = req.app.locals.username;
+            res.render("client/referral", {
+              layout: "client",
+              title: "EnkryptFinance | Referral",
+              referralLink: newReferralLink,
+              fullName,
+            });
+          })
+          .catch((err) => err);
+      })
+      .catch((err) => err);
+  });
 });
-router.get('/referral',(req,res,next)=>{
-
-    userModel.findOne({_id:req.session.access})
-        .then(user=>{
-            const newReferralLink = `http://:4000${req.baseUrl}/r/${user.fullName.split(' ')[0]}`;
-            const newReferral =  referralModel({link:newReferralLink,owner:req.session.access});
-                newReferral.save()
-                    .then(ref=>{
-                        user.referralUrl = newReferralLink;
-                        user.save()
-                            .then(r=>{
-                                const fullName = req.app.locals.username;
-                                res.render('client/referral',{layout: 'client', title:'EnkryptFinance | Referral',referralLink:newReferralLink,fullName});
-                            })
-                            .catch(err=>err)
-
-                    })
-                    .catch(err=>err)
-
-        })
-
+router.post("/referral/em/", (req, res, next) => {
+  const params = {
+    senderName: "Okibe Obinna",
+    sender: "EnkryptFinance",
+    client: req.body.email,
+    text: `Hi ${req.body.email}, welcome enkryptFinance`,
+    subject: `Welcome to enkryptFinance`,
+    user: user.fullName,
+    data: {
+      header: `Hi ${req.body.email}! welcome enkryptFinance`,
+      body: "You have been invited to join Enkrypt Finance",
+      link: req.body.refLink,
+    },
+    req,
+    res,
+  };
+  sendEmail(params);
 });
-router.post('/referral/em/',(req,res,next)=>{
-    const params = {
-        senderName: "Okibe Obinna",
-        sender: "EnkryptFinance",
-        client:  req.body.email,
-        text: `Hi ${req.body.email}, welcome enkryptFinance`,
-        subject: `Welcome to enkryptFinance`,
-        user: user.fullName,
-        data: {
-            header: `Hi ${req.body.email}! welcome enkryptFinance` ,
-            body:"You have been invited to join Enkrypt Finance",
-           link: req.body.refLink
-
-
-
-        },
-        req,
-        res
-
-    };
-      sendEmail(params)
-});
-
 
 ///REFERRAL URL
 
-router.get('/withdraw',(req,res,next)=>{
-    const fullName = req.app.locals.username;
-    res.render('client/withdraw',{layout: 'client', title:'EnkryptFinance | wallet',fullName});
+router.get("/withdraw", (req, res, next) => {
+  const fullName = req.app.locals.username;
+  res.render("client/withdraw", {
+    layout: "client",
+    title: "EnkryptFinance | wallet",
+    fullName,
+  });
 });
-router.get('/upgrade',(req,res,next)=>{
-    const fullName = req.app.locals.username;
-    const userId = req.session.access;
-    planModel.find({})
-        .lean()
-        .then(plan=>{
-            res.render('client/upgrade',{layout: 'client', title:'EnkryptFinance | Upgrade Plan',fullName, plan,userId });
-        })
+router.get("/upgrade", (req, res, next) => {
+  const fullName = req.app.locals.username;
+  const userId = req.session.access;
+  planModel
+    .find({})
+    .lean()
+    .then((plan) => {
+      res.render("client/upgrade", {
+        layout: "client",
+        title: "EnkryptFinance | Upgrade Plan",
+        fullName,
+        plan,
+        userId,
+      });
+    });
 });
-router.post('/upgrade/:id',(req,res,next)=>{
-    const fullName = req.app.locals.username;
-    console.log(req.params.id);
-    userModel.findById(req.params.id)
-        .then(user=>{
-           planModel.findOne({title:req.body.plan})
-               .then(plan=>{
-                   console.log(plan);
-                   user.plan = plan._id;
+router.post("/upgrade/:id", (req, res, next) => {
+  const fullName = req.app.locals.username;
+  console.log(req.params.id);
+  userModel
+    .findById(req.params.id)
+    .then((user) => {
+      planModel
+        .findOne({ title: req.body.plan })
+        .then((plan) => {
+          console.log(plan);
+          user.plan = plan._id;
 
-                   user.save()
-                       .then(user=>{
-                           res.redirect('/client')
-                       })
-                       .catch(err=>res.status(403).send(`${err} in user`))
-               })
-               .catch(err=>res.status(403).send(`${err} in plan`))
+          user
+            .save()
+            .then((user) => {
+              res.redirect("/client");
+            })
+            .catch((err) => res.status(403).send(`${err} in user`));
         })
-        .catch(err=>res.status(403).send(err))
+        .catch((err) => res.status(403).send(`${err} in plan`));
+    })
+    .catch((err) => res.status(403).send(err));
 });
-router.get('/editPassword',(req,res,next)=>{
-    const fullName = req.app.locals.username;
-    res.render('client/editPassword',{layout: 'client', title:'EnkryptFinance | Upgrade Plan',fullName});
+router.get("/editPassword", (req, res, next) => {
+  const fullName = req.app.locals.username;
+  res.render("client/editPassword", {
+    layout: "client",
+    title: "EnkryptFinance | Upgrade Plan",
+    fullName,
+  });
 });
-router.get('/logout',(req,res,next)=>{
-    req.session.destroy();
-    res.redirect('/auth/login');
+router.get("/logout", (req, res, next) => {
+  req.session.destroy();
+  res.redirect("/auth/login");
 });
 
 module.exports = router;
