5e2214f6426dc514006636ceed8d1be3afe1f930
mr-emeka <speak2c.emeka@gmail.com>
2021-05-28 22:26:21 +0100

    fixing ui issues

--- routes/emailroute.js
+++ routes/emailroute.js
@@ -1,11 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // const nodemailer = require('../helpers/nodemailer');
 
-router.get('/test', async (req,res,next)=>{
-   // await nodemailer();
-    res.send('done');
-
+router.get("/test", async (req, res, next) => {
+  // await nodemailer();
+  res.send("done");
 });
-
 
 module.exports = router;
