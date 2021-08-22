a13900e544a0249270ba891d65ee251f725c6598
NEXTink <okechukwuomeh3@gmail.com>
2021-08-22 17:44:39 +0100

    theme

--- public/js/liveCryptoTable.js
+++ public/js/liveCryptoTable.js
@@ -1,17 +1,17 @@
-const test = {
-    "currency": "BTC",
-    "id": "BTC",
-    "status": "active",
-    "price": "8451.36516421",
-    "price_date": "2019-06-14T00:00:00Z",
-    "price_timestamp": "2019-06-14T12:35:00Z",
-    "symbol": "BTC",
-    "circulating_supply": "17758462",
-    "max_supply": "21000000",
-    "name": "Bitcoin",
-    "logo_url": "https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg",
-}
-
+// const test = {
+//     "currency": "BTC",
+//     "id": "BTC",
+//     "status": "active",
+//     "price": "8451.36516421",
+//     "price_date": "2019-06-14T00:00:00Z",
+//     "price_timestamp": "2019-06-14T12:35:00Z",
+//     "symbol": "BTC",
+//     "circulating_supply": "17758462",
+//     "max_supply": "21000000",
+//     "name": "Bitcoin",
+//     "logo_url": "https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg",
+// }
+const socket = io();
 const tableRow = function (data) {
    let textColor = 'success';
    let icon = 'las la-arrow-up';
@@ -32,7 +32,7 @@ const tableRow = function (data) {
 
    return  `
   <tr>             <td class="h6 bold">${data.rank}</td>
-                    <td class="h6 bold"><img class="crypto-icons" src="${data.logo_url}"/></td>
+                    <td class="h6 bold"><img alt="${data.logo_url}" class="crypto-icons" src="${data.logo_url}"/></td>
                            <td>${data.symbol}</td>
                     <td class="h6 bold">${data.name}</td>
                   
@@ -40,7 +40,7 @@ const tableRow = function (data) {
                     <td class="h5 bold">$${price}</td>
                     <td class="h6 bold">$${marketCp}</td>
                     <td class="h6 bold">${supply}</td>
-                    <td class="${textColor} h5 bold"><i class="${icon}"></i> ${data['1d'].price_change_pct}</td>
+                    <td class="${textColor} h5 bold"><i class="${icon}"></i> ${((data['1d'].price_change_pct) * 100).toString().slice(0,8)}%</td>
 
              
                    
